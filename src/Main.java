a1101c774b1795376748d58be78075d724442e66
Amelia French <126724144+ameliafrench08@users.noreply.github.com>
2024-09-17 10:30:06 -0400

    Update Main.java
    Created a program where you enter a bill, tip percentage, and number of people, and the code prints out the total tip, total bill + tip, tip per person, and the total per person.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,82 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
-        
+        // int numPeople = 1;
+
+        System.out.println("________________________________________________________");
+        System.out.println("In the following program: DO NOT USE SYMBOLS");
+        System.out.println("________________________________________________________");
+
+        System.out.println();
+
+        System.out.print("Enter the total bill: $");
+        String totalBillStr = s.nextLine();
+        double totalBill = Double.parseDouble(totalBillStr);
+
+        System.out.println();
+        System.out.println();
+
+        System.out.print("Enter the tip percentage: %");
+        String tipPercentStr = s.nextLine();
+        int tipPercentage = Integer.parseInt(tipPercentStr);
+
+        System.out.println();
+        System.out.println();
+
+        System.out.print("Enter the number of people: ");
+        String totalPplStr = s.nextLine();
+        int numPeople = Integer.parseInt(totalPplStr);
+
+        System.out.println();
+
+        System.out.println("________________________________________________________");
+
+        double totalTip = (double) tipPercentage/100;
+        totalTip *= totalBill;
+        totalTip = Math.round(totalTip * 100.0) / 100.0;
+
+        double billAndTip = totalTip + totalBill;
+
+        double tipPerPerson = totalTip/numPeople;
+        tipPerPerson = Math.round(tipPerPerson * 100.0) / 100.0;
+
+        double totalPerPerson = billAndTip / numPeople;
+        totalPerPerson = Math.round(totalPerPerson * 100.0)/100.0;
+
+        billAndTip = Math.round(billAndTip * 100.0) / 100.0;
+
+        System.out.println();
+        System.out.println("Total tip: $" + totalTip);
+        System.out.println();
+
+        System.out.println("________________________________________________________");
+
+        System.out.println();
+        System.out.println("Total bill + tip: $" + billAndTip);
+        System.out.println();
+
+        System.out.println("________________________________________________________");
+
+        System.out.println();
+        System.out.println("Tip per person: $" + tipPerPerson);
+        System.out.println();
+
+        System.out.println("________________________________________________________");
+
+        System.out.println();
+        System.out.println("Total per person: $" + totalPerPerson);
+        System.out.println();
+
+        System.out.println("________________________________________________________");
+
+
+
+
+
     }
 }
